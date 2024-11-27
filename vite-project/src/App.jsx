e3984cfd474de3061cfb7e6d2e133c8330e5b9a1
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-27 17:23:14 +0100

    add taskfilter completed to completed/in progress

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,5 +2,8 @@ import { tasks } from "./data/tasksData";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+function App() {
+  const completedTasks = tasks.filter((task) => task.state === "completed");
+  const inProgressTasks = tasks.filter((task) => task.state !== "completed");
 
-export default App
+  export default App
