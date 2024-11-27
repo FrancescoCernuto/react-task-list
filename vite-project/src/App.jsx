9448c59cf437c9ab6e31126db5051e627a0c8dea
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-27 17:38:08 +0100

    add graphic

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,6 +8,34 @@ function App() {
 
   function generateList(list) {
     return (
+      <ul className="list-unstyled">
+        {list.map((task, index) => (
+          <li key={index}>
+            <span className="fw-semibold">
+              <span className="fw-bold">{task.title}</span>
+              {task.state === "completed" ? (
+                <span class="badge rounded-pill text-bg-success text-capitalize mx-2">
+                  {task.state}
+                </span>
+              ) : task.state === "backlog" ? (
+                <span class="badge rounded-pill text-bg-secondary text-capitalize mx-2">
+                  {task.state}
+                </span>
+              ) : (
+                <span class="badge rounded-pill text-bg-warning text-capitalize mx-2">
+                  {task.state}
+                </span>
+              )}
+              <br />
+              Priority: {task.priority}
+              <br />
+              Est. Time: {task.estimatedTime}
+            </span>
+          </li>
+        ))}
+      </ul>
+    );
+  }
 
 
   export default App
